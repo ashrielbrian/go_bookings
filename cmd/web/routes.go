992671c182ad52960887eb2cd625e062d0ca3fb0
Brian Tang <ashrielbrian@gmail.com>
2022-02-17 15:23:30 +0800

    adds static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,11 +16,12 @@ func routes(a *config.AppConfig) http.Handler {
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
-	// mux := pat.New()
-	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	// serve static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
