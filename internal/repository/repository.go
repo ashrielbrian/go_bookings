28824cddce5e2e5a5cbce94820dc77348d2bd4b8
Brian Tang <ashrielbrian@gmail.com>
2022-02-22 22:34:43 +0800

    Adds database functions

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,14 @@
 package repository
 
+import (
+	"time"
+
+	"github.com/ashrielbrian/go_bookings/internal/models"
+)
+
 type DatabaseRepo interface {
 	AllUsers() bool
+	InsertReservation(res models.Reservation) (int, error)
+	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
 }
